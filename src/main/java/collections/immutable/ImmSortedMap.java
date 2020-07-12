5ceb394efdd62b1e18d4cafb5f67b45925be763d
egoff <egoff@egoff-c02zm2q6md6x.lan>
2020-07-11 22:08:08 -0500

    docs

--- src/main/java/collections/immutable/ImmSortedMap.java
+++ src/main/java/collections/immutable/ImmSortedMap.java
@@ -31,13 +31,13 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
      * Returns a sorted map with all entries &gt;= the given key. 
      * <p>This operation is O(log n).
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).ge(2).equals(ImmCollections.asSortedMap(4,1,  2,2,  3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).ge(4).equals(ImmCollections.asSortedMap(4,1));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).ge(0).equals(ImmCollections.asSortedMap(4,1,  2,2,  3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).ge(5).equals(ImmCollections.emptySortedMap());
-     * </pre>
+     * }</pre>
      * @param key The min key
      * @return a sorted map with all entries &gt;= the given key
      */
@@ -46,13 +46,13 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
     /**
      * Returns a sorted map with all entries &lt; the given key. 
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).lt(2).equals(ImmCollections.emptySortedMap());
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).lt(4).equals(ImmCollections.asSortedMap(2,2,  3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).lt(0).equals(ImmCollections.emptySortedMap());
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).lt(5).equals(ImmCollections.asSortedMap(4,1,  2,2,  3,3));
-     * </pre>
+     * }</pre>
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
      * @param key The max key
      * @return a sorted map with all entries &lt; the given key
@@ -62,13 +62,12 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
     /**
      * Returns a sorted map with no keys between lowKey and highKey.
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).exclude(2, 3).equals(ImmCollections.asSortedMap(3,3, 4,1));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).exclude(2, 4).equals(ImmCollections.asSortedMap(4,1));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).exclude(0, 4).equals(ImmCollections.asSortedMap(4,1));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).exclude(0, 5).equals(ImmCollections.emptySortedMap());
-     * </pre>
+     * }</pre>
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
      * @param lowKey The min key.  (inclusive)
      * @param highKey The max key.  (exclusive)
@@ -79,14 +78,14 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
     /**
      * Returns a sorted map with all keys between lowKey and highKey.
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).subSet(2, 3).equals(ImmCollections.asSortedMap(2,2));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).subSet(2, 4).equals(ImmCollections.asSortedMap(2,2,   3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).subSet(0, 4).equals(ImmCollections.asSortedMap(2,2,   3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).subSet(0, 5).equals(ImmCollections.asSortedMap(2,2,   3,3,   4,1));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).subSet(3, 3).equals(ImmCollections.emptySortedMap());
-     * </pre>
+     * }</pre>
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
      * @param lowKey The min key.  (inclusive)
      * @param highKey The max key.  (exclusive)
@@ -97,10 +96,10 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
 	/**
 	 * Returns a sorted map with all elements reversed.
      * <p>This operation is O(1).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(2,2,  3,3,  4,1).reversed().equals(ImmCollections.asSortedMap(4,1,  3,3,   2,2));
-     * </pre>
+     * }</pre>
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
 	 * @return a sorted map with all elements reversed
 	 */
@@ -109,12 +108,12 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
 	/**
 	 * Returns the index of the given key.  Returns -1 if not found.
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).indexOfKey(2) == 0;
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).indexOfKey(4) == 2;
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).indexOfKey(5) == -1;
-     * </pre>
+     * }</pre>
 	 * @param key The key to get an index of.
 	 * @return the index of the given key
 	 */
@@ -135,12 +134,12 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
     /**
      * Returns the entry at the given index.
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).getAt(0).getKey() == 2;
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).getAt(1).getKey() == 3;
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).getAt(2).getKey() == 4;
-     * </pre>
+     * }</pre>
      * @param index The index
      * @return  the entry at the given index
      */
@@ -149,12 +148,12 @@ public interface ImmSortedMap<K, V> extends ImmMap<K, V> {
     /**
      * Returns a map with the entry at the given index removed.
      * <p>This operation is O(log n).
-     * <pre>
-     * Example:
+     * <p>Example:
+     * <pre>{@code
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).removeAt(0).equals(ImmCollections.asSortedMap(4,1,  3,3));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).removeAt(1).equals(ImmCollections.asSortedMap(4,1,  2,2));
      *     assert ImmCollections.asSortedMap(4,1,  2,2,  3,3).removeAt(2).equals(ImmCollections.asSortedMap(3,3,  2,2));
-     * </pre>
+     * }</pre>
      * <p>*THIS OPERATION IS IMMUTABLE, THE PREVIOUS Map23 IS UNCHANGED!*.
      * @param index The index
      * @return a map with the entry at the given index removed
